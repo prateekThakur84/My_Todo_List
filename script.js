9f2086f60c81d2f89ffb1fc00740edd5b233582a
prateekThakur84 <142097726+prateekThakur84@users.noreply.github.com>
2024-06-19 22:40:18 +0530

    new feature added (task completed percentage)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const completionPercentage = document.getElementById("completion-percentage");
+
 
 // function to add our task
 
@@ -14,6 +16,8 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML= "\u00d7";
         li.appendChild(span);
+        document.getElementById("completion-percentage").style.display = "flex";
+        updatePercentage();
     }
     inputBox.value = '';
     saveData();
@@ -25,10 +29,12 @@ listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData();
+        updatePercentage();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
+        updatePercentage();
     }
 },false);
 
@@ -42,6 +48,19 @@ function saveData(){
 
     function showList(){
     listContainer.innerHTML = localStorage.getItem("data");
+    updatePercentage();
 };
 
+// calculate the percentage of how much task is completed
+function updatePercentage() {
+    const tasks = listContainer.getElementsByTagName("li");
+    const completedTasks = listContainer.getElementsByClassName("checked");
+    const totalTasks = tasks.length;
+    const totalCompleted = completedTasks.length;
+    const percentage = totalTasks === 0 ? 0 : Math.round((totalCompleted / totalTasks) * 100);
+    completionPercentage.innerHTML = `${percentage}% tasks completed`;
+}
+
 showList();
+
+
